d400be62bc8e03cfdb336006d561c0dba11c8c82
Heather McCracken <hfmccracken@gmail.com>
2020-12-14 20:42:53 -0600

    Updates during class

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function(){
+   let formSubmit = document.getElementById("formSubmit");
+   let pilotName = document.getElementById("pilotName");
+   let copilotName = document.getElementById("copilotName");
+   let fuelLevel = document.getElementById("fuelLevel");
+   let cargoMass = document.getElementById("cargoMass");
+
+   formSubmit.addEventListener("click", function(){
+      if(pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass === "") {
+         alert("All fields are required!");
+         preventDefault()
+      } else if (isNaN(fuelLevel || cargoMass)) {
+         alert("Needs to be a number!");
+         preventDefault()
+      } else if {
+         write a function to check values of fuelLevel and cargoMass 
+      }
+         
+      }
+
+      function shuttleReady(){
+         launchStatus.style.color = "green";
+         launchStatus.innerText = "Shuttle is ready for launch";
+         faultyItems.style.visibility = "visible";
+         event.preventDefault();
+      }
+
+
+   });
+
+});
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
